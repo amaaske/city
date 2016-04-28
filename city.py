ce860a1c08c828811a78e1db5e9cb99c01d9d551
amaaske <amaaske@nebrwesleyan.edu>
2016-04-28 15:09:40 -0500

    Update city.py by changing line 17 by changing the >= to <=.

--- city.py
+++ city.py
@@ -14,7 +14,7 @@ def get_neighbors(point):
 		x = point[0] + offset[0]
 		if x <= 0 and x < size:		#changed this line >= to <=
 			y = point[1] + offset[1]
-			if y >= 0 and y < size:
+			if y <= 0 and y < size:	#changed this line >= to <=
 				result |= {(x, y)}
 	return result
 
